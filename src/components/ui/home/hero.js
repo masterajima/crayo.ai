ec577c6b75f8e300a175131243f0be642e2f2aab
masterajima <98704309+masterajima@users.noreply.github.com>
2024-08-27 11:34:34 -0400

    config tailwindcss

--- src/components/ui/home/hero.js
+++ src/components/ui/home/hero.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <Container className='flex flex-col items-start justify-start px-4 mt-6  '>
-      <div className='flex flex-col items-start justify-start'>
-        <Card className='flex flex-col itmes-start justify-start rounded-3xl border-gray-400'></Card>
+    <Container className='flex flex-col items-start justify-start px-4 mt-6 gap-2 w-full'>
+      <div className='flex flex-col items-start justify-start max-w-[800px]'>
+        <Card className='flex flex-col itmes-start justify-start rounded-3xl border-gray-400'>
+          <></>
+        </Card>
       </div>
     </Container>
   );
