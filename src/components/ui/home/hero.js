eb758d93bc0d455b88ed11de53e6df64506a0508
masterajima <98704309+masterajima@users.noreply.github.com>
2024-08-27 10:53:46 -0400

    add card for hero section

---
+++ src/components/ui/home/hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <Container className='flex flex-col items-start justify-start px-4 mt-6  '>
+      <div className='flex flex-col items-start justify-start'>
+        <Card className='flex flex-col itmes-start justify-start rounded-3xl border-gray-400'></Card>
+      </div>
+    </Container>
+  );
+};
+
+export default Hero;
