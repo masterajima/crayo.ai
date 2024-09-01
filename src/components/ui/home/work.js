fa0cfe617b8c17fad256a62978d69e723c56f709
masterajima <98704309+masterajima@users.noreply.github.com>
2024-09-01 14:19:15 -0400

    add gradient in background

---
+++ src/components/ui/home/work.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WorkCrayo = () => {
+  return (
+    <div className='flex flex-col items-center justify-center md:items-start md:justify-start p-12 gap-10 '>
+      <div className='flex flex-col items-center md:items-start justify-center md:justify-start w-full '>
+        <div className='flex flex-col gap-10'>
+          <div className='flex px-6 rounded-full py-2 bg-gradient-to-br from-[#dbf0ff] to-[#dfe2ff] via-[#dfe2ff] p-2'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkCrayo;
