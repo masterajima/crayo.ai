08d4ad951b764a296327bc89600fde692ea0be76
masterajima <98704309+masterajima@users.noreply.github.com>
2024-09-02 11:21:13 -0400

    add heading

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from '@/components/ui/home/hero';
 import { Container } from '../components/ui/containers';
+import WorkCrayo from '@/components/ui/home/work';
 
 const Home = () => {
   return (
-    <Container className='flex justify-center items-center text-center w-full '>
+    <Container className='flex flex-col justify-center items-center text-center w-full '>
       <Hero />
+      <WorkCrayo></WorkCrayo>
     </Container>
   );
 };
--- src/components/ui/home/work.js
+++ src/components/ui/home/work.js
@@ -4,8 +4,23 @@ const WorkCrayo = () => {
   return (
     <div className='flex flex-col items-center justify-center md:items-start md:justify-start p-12 gap-10 '>
       <div className='flex flex-col items-center md:items-start justify-center md:justify-start w-full '>
-        <div className='flex flex-col gap-10'>
-          <div className='flex px-6 rounded-full py-2 bg-gradient-to-br from-[#dbf0ff] to-[#dfe2ff] via-[#dfe2ff] p-2'></div>
+        <div className='flex flex-col items-start gap-10'>
+          <div className='flex px-6 rounded-full py-2 bg-gradient-to-br from-[#dbf0ff] to-[#dfe2ff] via-[#dfe2ff] p-3'>
+            <div className='flex gap-2 '>
+              <div className='w-2 h-2 rounded-full  bg-[#000d49] '></div>
+              <div className='flex  items-center gap-6'>
+                <p className='text-[#000d49] font-medium text-sm text-center mx-auto'>
+                  How it Works
+                </p>
+                <div className='w-2 h-2 rounded-full  bg-[#000d49] '></div>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col items-center md:items-start gap-10'>
+            <h2 clssName='font-semibold text-4xl text-[#000d49]'>
+              From Ideas to Short Videos
+            </h2>
+          </div>
         </div>
       </div>
     </div>
