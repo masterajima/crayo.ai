982315b9b483b696b719c78606a9cc4947ba5b51
masterajima <98704309+masterajima@users.noreply.github.com>
2024-09-08 14:56:01 -0400

    set mobile and large screen view

--- src/components/ui/home/tools.js
+++ src/components/ui/home/tools.js
@@ -5,9 +5,9 @@ import Link from 'next/link';
 
 const ToolsPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center p-12  w-full'>
-      <Card className='flex flex-col items-center justify-center rounded-3xl border-gray-800  bg-gray-900 p-18 w-full h-full'>
-        <div className='flex flex-col items-center justify-center mt-8'>
+    <div className='flex flex-col md:items-center md:justify-center lg:p-12  w-full'>
+      <Card className='flex flex-col lg:items-start lg:justify-start md:items-center md:justify-center rounded-3xl border-gray-800  bg-gray-900 lg:p-18 w-full h-full'>
+        <div className='flex flex-col items-start justify-start md:items-center md:justify-center mt-8'>
           <Link
             href='/'
             className='flex bg-[#ffffff0f] items-center justify-center p-2 rounded-full px-3 border-white'>
@@ -18,8 +18,8 @@ const ToolsPage = () => {
             </div>
           </Link>
           <div className=' gap-4 flex flex-col items-center mt-8'>
-            <h2 className='w-[350px] text-[35px] font-semibold text-white'>
-              All in one tool for{' '}
+            <h2 className='md:w-[350px] lg:w-full text-[35px] font-semibold text-white'>
+              All in one tool for Genrating
               <span className='text-white text-[35px]'>viral Shorts using AI</span>
             </h2>
             <p className='text-[15px] text-white'>
@@ -81,7 +81,7 @@ const ToolsPage = () => {
               </Link>
             </div>
           </div>
-          <Card className='flex  items-center justify-center rounded-3xl bg-[#ffffff0f] border-[#e9f0ff1a] h-[540px]  w-2/3 mt-8 gap-12 mb-8'>
+          <Card className='flex  items-center justify-center rounded-3xl bg-[#ffffff0f] border-[#e9f0ff1a] lg:h-[540px]  w-full lg:w-2/3 lg:mt-8 gap-12 mb-8'>
             <div className='flex flex-col items-start'>
               <div className='flex itmems-start p-2 rounded-full px-3 gap-2 bg-[#e9f0ff0a] border-[#9ab6ff] border-2 rotate-[-0.0522561rad] -translate-x-1/2 -translate-y-1/2 mb-4'>
                 <div className='h-[33px] w-[33px]'>
@@ -138,6 +138,18 @@ const ToolsPage = () => {
                 </div>
                 <span className='text-white text-lg font-[Inter] tracking-tight'>
                   Background Video
+                </span>
+              </div>
+              <div className='flex items-center p-2 rounded-full  gap-2 bg-[#9ab7ff1a] border-[#9ab6ff] border-2 rotate-[0.000354249rad] translate-x-1/2 translate-y-1/2 px-6'>
+                <div className='h-[24px] w-[24px]'>
+                  <div className='w-full h-full  bg-white'>
+                    <svg style={{ width: '7', height: '7' }}>
+                      <use href='#svg1124643241'></use>
+                    </svg>
+                  </div>
+                </div>
+                <span className='text-white text-lg font-[Inter] tracking-tight '>
+                  Manual Editing
                 </span>
               </div>
             </div>
