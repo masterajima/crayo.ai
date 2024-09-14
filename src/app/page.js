ad51b916bd5107408f79101959cd67cb4224e441
masterajima <98704309+masterajima@users.noreply.github.com>
2024-09-14 14:31:28 -0400

    add testimonial card

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,54 @@ import FunctionPage from '@/components/ui/home/function';
 import TestimonialPage from '@/components/ui/home/testimonial';
 
 const Home = () => {
+  const testimonialData = {
+    title: 'Our Customer ❤️ Us',
+    subtitle:
+      'Create unlimited shorts videos at once.Auto genrate captions,effects,background and music',
+    testimonials: [
+      {
+        name: 'Daniel',
+        role: 'Content Creator',
+        content:
+          'I can make over 50 clips a day many of which will go viral. Ive never seen anything like this before.',
+        imageSrc: '/assets/images/img.avif',
+      },
+      {
+        name: 'Sid',
+        role: 'Content Creator',
+        content:
+          "Crayo AI is incredible! I just type in my ideas and it generates a complete TikTok video. It's like magic – full videos with just text input!",
+        imageSrc: '/assets/images/img.avif',
+      },
+      {
+        name: 'Moe',
+        role: 'Content Creator',
+        content:
+          'Crayo helped us 4x our content output The AI UGC creators on Crayo are the best AI video feature ever!',
+        imageSrc: '/assets/images/img.avif',
+      },
+      {
+        name: 'Aleem',
+        role: 'Content Creator',
+        content:
+          'The AI voiceovers on Crayo AI are game-changing. They add a professional touch to my videos, making them stand out on TikTok.',
+        imageSrc: '/assets/images/img.avif',
+      },
+      {
+        name: 'Arib',
+        role: 'Content Creator',
+        content:
+          'The AI voiceovers on Crayo AI are game-changing. They add a professional touch to my videos, making them stand out on TikTok.',
+        imageSrc: '/assets/images/img.avif',
+      },
+      {
+        name: 'Musa',
+        role: 'Content Creator',
+        content: 'This is going to become the biggest clipping tool in 2024. Trust me.',
+        imageSrc: '/assets/images/img.avif',
+      },
+    ],
+  };
   return (
     <div className='flex flex-col justify-start items-start text-center px-6 w-full '>
       <Hero />
@@ -14,7 +62,7 @@ const Home = () => {
       <FeaturesPage />
       <ToolsPage></ToolsPage>
       <FunctionPage></FunctionPage>
-      <TestimonialPage></TestimonialPage>
+      <TestimonialPage {...testimonialData} />
     </div>
   );
 };
