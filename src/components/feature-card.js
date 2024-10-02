b11e3d5b4d3cf7697c7bddafe37f59bf19f5dd5f
masterajima <masterajima585@gmail.com>
2024-10-02 11:06:04 -0400

    add reusable feature card component

--- src/components/feature-card.js
+++ src/components/feature-card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card } from '@/shadcn/ui/card';
 import Image from 'next/image';
 
-const FeatureCard = ({ imageSrc, title }) => {
+const FeatureCard = ({ imageSrc, title, description }) => {
   return (
     <Card className='flex flex-col items-start justify-start bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8 p-4 w-full relative h-full'>
       <div className='absolute top-0 right-0'>
@@ -13,6 +13,12 @@ const FeatureCard = ({ imageSrc, title }) => {
           height={189}
           className='object-cover object-top-right'
         />
+      </div>
+      <div className='flex flex-col items-start justify-start gap-4 w-full mt-[200px]'>
+        <h3 className='text-[#000d49] text-xl md:text-[17px] font-bold'>{title}</h3>
+        <p className='text-sm md:text-[15px] text-[#000d49] w-full md:w-2/3'>
+          {description}
+        </p>
       </div>
     </Card>
   );
