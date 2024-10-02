104f857608f65d71d83e365a618cdbc9a5804da1
masterajima <masterajima585@gmail.com>
2024-10-02 11:15:45 -0400

    set card size for mobile device

--- src/components/ui/home/function.js
+++ src/components/ui/home/function.js
@@ -2,9 +2,47 @@ import React from 'react';
 import Link from 'next/link';
 import { Card } from '@/shadcn/ui/card';
 import Image from 'next/image';
+import FeatureCard from '@/components/feature-card';
 const FunctionPage = () => {
+  const features = [
+    {
+      imageSrc: '/assets/images/insta.avif',
+      title: 'Instagram Reels',
+      description:
+        'Turn Instagram reels into exciting content by adding gameplay to keep viewers entertained.',
+    },
+    {
+      imageSrc: '/assets/images/tiktok.avif',
+      title: 'TikTok Videos',
+      description:
+        'Use AI to make money from the CPB program by creating entertaining content using Crayo.',
+    },
+    {
+      imageSrc: '/assets/images/cam.avif',
+      title: 'Faceless Content',
+      description: 'Create a video using just a script and create captivating content.',
+    },
+    {
+      imageSrc: '/assets/images/reddit.avif',
+      title: 'Reddit Stories',
+      description:
+        'Use AI to take a Reddit story and turn it into an AI generated video.',
+    },
+    {
+      imageSrc: '/assets/images/youtube.avif',
+      title: 'YouTube Shorts',
+      description:
+        'Turn Instagram reels into exciting content by adding gameplay to keep viewers entertained.',
+    },
+    {
+      imageSrc: '/assets/images/notes.avif',
+      title: 'Instagram Reels',
+      description:
+        'Turn Instagram reels into exciting content by adding gameplay to keep viewers entertained.',
+    },
+  ];
   return (
-    <div className='flex flex-col items-start justify-start bg-[#ffffff] p-[50px] w-full gap-4'>
+    <div className='flex flex-col items-start justify-start bg-[#ffffff] md:p-[50px] w-full gap-4'>
       <div className='flex items-start justify-start rounded-full p-2 gap-3 bg-gradient-to-r from-[#dbf0ff] via-[#f6dfff] to-[#dfe2ff] border-[#ffffff80]'>
         <div className='bg-[#000d49] rounded-full w-3 h-3'></div>
         <p className='text-[16px] font-[Inter] text-[#000d49]'>How it Works</p>
@@ -76,143 +114,11 @@ const FunctionPage = () => {
         </div>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-8  '>
-        <Card className='flex flex-col items-start justify-start bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8  relative w-full'>
-          <div className=' flex flex-col  border-[#e9f0ff] bg-[#f2f6ff]'>
-            <div className='absolute  top-0 right-0'>
-              <Image
-                src='/assets/images/insta.avif'
-                alt='insta'
-                width={189}
-                height={189}
-                className='object-cover object-top-right'
-              />
-            </div>
-          </div>
-
-          <div className='flex flex-col items-start justify-strat gap-4 mt-[200px] px-4 w-full'>
-            <h3 className='text-[#000d49] md:text-[17px] text-xl font-bold'>
-              Instagram Reels
-            </h3>
-
-            <p className='md:text-[15px] text-sm text-[#000d4980] md:w-2/3 w-full '>
-              Turn Instagram reels into exciting conetent by adding gameplay to keep
-              viewers entertained.{' '}
-            </p>
-          </div>
-        </Card>
-        <Card className='flex flex-col items-start justify-start bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8 relative'>
-          <div className=' top-0 right-0 absolute'>
-            <Image
-              src='/assets/images/tiktok.avif'
-              alt='insta'
-              width={189}
-              height={189}
-              className='object-cover object-top-right'
-            />
-          </div>
-
-          <div className='flex flex-col items-start gap-4 mt-[200px] px-4'>
-            <h3 className='text-[#000d49] md:text-[17px] text-xl font-bold'>
-              TikTok Videos
-            </h3>
-
-            <p className='md:text-[15px] text-sm text-[#000d4980] md:w-2/3'>
-              Use AI to make money from the CPB program by creating entertaining content
-              using Crayo.
-            </p>
-          </div>
-        </Card>
-        <Card className='flex flex-col items-start justify-start bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8 relative'>
-          <div className=' top-0 right-0  absolute'>
-            <Image
-              src='/assets/images/cam.avif'
-              alt='insta'
-              width={189}
-              height={189}
-              className='object-cover object-top-right'
-            />
-          </div>
-
-          <div className='flex flex-col items-start justify-start mt-[200px] gap-4 px-4'>
-            <h3 className='text-[#000d49] md:text-[17px] text-xl font-bold'>
-              Faceless Content
-            </h3>
-
-            <p className='text-[15px] text-[#000d4980] text-sm md:w-2/3'>
-              Create a video using just a script and create captivating content.
-            </p>
-          </div>
-        </Card>
-      </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full'>
-        <Card className='flex flex-col items-start justify-start bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8 relative w-full p-4 '>
-          <div className='absolute top-0 right-0 '>
-            <Image
-              src='/assets/images/reddit.avif'
-              alt='insta'
-              width={189}
-              height={189}
-              className='object-cover object-top-right'
-            />
-          </div>
-
-          <div className='flex flex-col items-start justify-start mt-[200px] gap-4 p-4'>
-            <h3 className='text-[#000d49] md:text-[17px] text-xl font-bold'>
-              Reddit Stories
-            </h3>
-
-            <p className='md:text-[15px] text-sm text-[#000d4980] md:w-2/3'>
-              Use AI to take a Reddit story and turn it into an AI generated video.
-            </p>
-          </div>
-        </Card>
-        <Card className='flex flex-col bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8 relative'>
-          <div className='absolute top-0 right-0'>
-            <Image
-              src='/assets/images/youtube.avif'
-              alt='insta'
-              width={189}
-              height={189}
-              className='object-cover object-top-right'
-            />
-          </div>
-
-          <div className='flex flex-col items-start justify-start mt-[200px] gap-4'>
-            <h3 className='text-[#000d49] md:text-[17px] text-xl font-bold'>
-              YouTube Shorts
-            </h3>
-
-            <p className='md:text-[15px] text-sm text-[#000d4980] md:w-2/3'>
-              Turn Instagram reels into exciting content by adding gameplay to keep
-              viewers entertained.
-            </p>
-          </div>
-        </Card>
-
-        <Card className='flex flex-col items-start justify-start bg-[#f2f6ff] border-[#f2f6ff] rounded-3xl lg:p-8 relative'>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='absolute top-0 right-0'>
-              <Image
-                src='/assets/images/notes.avif'
-                alt='insta'
-                width={189}
-                height={189}
-                className='object-cover object-top-right'
-              />
-            </div>
-
-            <div className='flex flex-col items-start justify-start mt-[200px] gap-4 '>
-              <h3 className='text-[#000d49] md:text-[17px] text-xl font-bold'>
-                Instagram Reels
-              </h3>
-
-              <p className='md:text-[15px] text-sm  text-[#000d4980] md:w-2/3'>
-                Turn Instagram reels into exciting content by adding gameplay to keep
-                viewers entertained.
-              </p>
-            </div>
-          </div>
-        </Card>
+        {features.map((feature, index) => (
+          <FeatureCard
+            key={index}
+            {...feature}></FeatureCard>
+        ))}
       </div>
     </div>
   );
